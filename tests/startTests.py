a6741d85cd5369c3d93e985f39e2756aa895f186
theotherp@gmx.de <SFSu3uR#qe&n#Y7lBC*J>
2017-04-27 18:37:40 +0200

    Update to 0.2.217

--- tests/startTests.py
+++
@@ -1,9 +0,0 @@
-from os.path import dirname, join, abspath
-from sys import path
-import pytest
-
-base_path = dirname(dirname(dirname(abspath(__file__))))
-path.insert(0, join(base_path, 'nzbhydra'))
-path.insert(0, join(base_path, 'libs'))
-print(path)
-pytest.main("--capture=sys")
